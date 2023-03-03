811ccd5188d58d1fa91cd6b63d9388722dfb8193
OCTEST21 <kahasocihal@gmail.com>
2023-03-03 12:05:45 +0100

    create nav component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import FicheLogement from "./pages/FicheLogement/FicheLogement";
+import Error from "./pages/Error/Error";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
-   
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logement/:id" element={<FicheLogement />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
